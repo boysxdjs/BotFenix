469e0a94a2a468ccc8bb8fd8ab6c423e3cb0d44c
xxddbotsx <149536321+boysxdjs@users.noreply.github.com>
2023-12-03 00:09:47 -0500

    🦂 𝘼𝙡𝙖𝙘𝙧𝙖𝙣 🦂
    🦂 𝘼𝙡𝙖𝙘𝙧𝙖𝙣 🦂

---
+++ plugins/owner-ax.js
@@ -0,0 +1,12 @@
+let handler = (m) => m;
+handler.all = async function (m) {
+
+
+  if (/^yhair|owner|cristhofer$/i.test(m.text)) {
+    var ax = `𝙈𝙞 𝘾𝙧𝙚𝙖𝙙𝙤𝙧 𝙀𝙨 𝘾𝙧𝙞𝙨𝙩𝙝𝙤𝙛𝙚𝙧 𝙎𝙞 𝘿𝙚𝙨𝙚𝙖𝙨 𝘾𝙤𝙢𝙥𝙧𝙖𝙧 𝙀𝙨𝙩𝙚 𝘽𝙤𝙩 𝙎𝙪 𝙉𝙪𝙢𝙚𝙧𝙤 𝙀𝙨 : @${global.suittag}`;
+
+
+    m.reply(ax, m.chat, { mentions: conn.parseMention(ax)}); 
+  return !0
+}}
+export default handler
