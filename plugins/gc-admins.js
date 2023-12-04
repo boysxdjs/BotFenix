8af1b0e5cab8efb782f4004ef80114c1896c391c
xxddbotsx <149536321+boysxdjs@users.noreply.github.com>
2023-12-03 17:53:43 -1200

    🦅 𝘽𝙤𝙩 𝙁𝙚𝙣𝙞𝙭 🦅

--- plugins/gc-admins.js
+++ plugins/gc-admins.js
@@ -4,14 +4,14 @@ const handler = async (m, {conn, participants, groupMetadata, args}) => {
   const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n');
   const owner = groupMetadata.owner || groupAdmins.find((p) => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net';
   const pesan = args.join` `;
-  const oi = `🩵 𝘽𝙪𝙣𝙣𝙮 𝘽𝙤𝙩 🩵 ${pesan}`;
+  const oi = `🦅 𝘽𝙤𝙩 𝙁𝙚𝙣𝙞𝙭 🦅 ${pesan}`;
   const text = `
 ${oi}
 𝙎𝙞 𝘿𝙚𝙨𝙚𝙖𝙨 𝙀𝙡 𝘽𝙤𝙩 𝘾𝙤𝙢𝙥𝙧𝙖 𝘾𝙤𝙣 wa.me/51906582530
 
 ${listAdmin}
 
-🩵 𝘽𝙪𝙣𝙣𝙮 𝘽𝙤𝙩 🩵`.trim();
+🦅 𝘽𝙤𝙩 𝙁𝙚𝙣𝙞𝙭 🦅`.trim();
   conn.sendFile(m.chat, pp, 'error.jpg', text, m, false, {mentions: [...groupAdmins.map((v) => v.id), owner]});
 };
 handler.help = ['admins <texto>'];
