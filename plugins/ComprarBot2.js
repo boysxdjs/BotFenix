32c5bd530c5a6e2d9a199e7822f21da879b9beb2
xxddbotsx <149536321+boysxdjs@users.noreply.github.com>
2024-01-21 00:58:55 +0100

    Add files via upload

---
+++ plugins/ComprarBot2.js
@@ -0,0 +1,29 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.ComprarBot2);
+};
+handler.command = /^(ComprarBot2)$/i;
+export default handler;
+global.ComprarBot2 = `
+
+‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ *Compra con Nosotros*‎ ‎ ‎‎  ‎  *Optimus Bot 🤖*‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎
+
+‎ ‎ ‎🫰 *4* soles por grupo 
+
+Adquiriendo el bot para grupos sería por ( *1 mes* ) de ahí tendrá que renovar
+
+
+ 🫰 *22* Soles Via Termux
+
+ ‎Bot Perzonalizado A Tu Gusto , La Instalacion Es Via Apk Bot *Permanente*
+
+
+  🫰 *30* Soles Via Pagina + *10* Soles Al Mes
+
+ ‎Bot Perzonalizado Sin Necesidad De Instalarlo Tu , Bot.*Mensual* 
+
+• *Tiempo Max De Entrega 2 Dias*
+• *Fines De Semana Estoy Off*
+• *Pregunta Cuanto Es En Tu Pais Al Wa.me/51902360712*
+• *Recuerda Mandar Mensaje A Wa.me/51902360712 Si Estas Interesado*
+• *Recuerda Que Este Chat Solo Es Un Bot El Dueño Oficial Es Wa.me/51902360712*
+`;
