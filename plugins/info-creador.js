11f26775533b296f36799e5bbd97e0df2e3d944c
xxddbotsx <149536321+boysxdjs@users.noreply.github.com>
2024-01-22 18:30:10 +0100

    Update info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,9 +1,9 @@
 const handler = async (m, {conn, usedPrefix}) => {
   const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
   const document = doc[Math.floor(Math.random() * doc.length)];
-  const text = `*Ey Hola Como Estas 😻 ?\nSi Deseas Comprar El Bot Su Numero Es : +51 906 528 530 Escribele Un .yape`.trim();
+  const text = `*Ey Hola Como Estas 😻 ?\nSi Deseas Comprar El Bot Su Numero Es : +51 926 933 818 `.trim();
   const buttonMessage= {
-    'document': {url: `https://www.instagram.com/team.clan.hack/`},
+    'document': {url: `https://www.instagram.com/optimus.ventas.bot`},
     'mimetype': `application/${document}`,
     'fileName': `「  𝘼𝙡𝙖𝙘𝙧𝙖𝙣 」`,
     'fileLength': 99999999999999,
@@ -12,13 +12,13 @@ const handler = async (m, {conn, usedPrefix}) => {
       'forwardingScore': 200,
       'isForwarded': true,
       'externalAdReply': {
-        'mediaUrl': 'https://www.instagram.com/team.clan.hack/',
+        'mediaUrl': 'https://www.instagram.com/optimus.ventas.bot',
         'mediaType': 2,
         'previewType': 'pdf',
         'title': '🦂 𝘼𝙡𝙖𝙘𝙧𝙖𝙣 🦂',
         'body': wm,
         'thumbnail': imagen1,
-        'sourceUrl': 'https://www.instagram.com/team.clan.hack/'}},
+        'sourceUrl': 'https://www.instagram.com/optimus.ventas.bot/'}},
     'caption': text,
     'footer': wm,
     // 'buttons':[
